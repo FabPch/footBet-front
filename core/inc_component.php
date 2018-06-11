158bf96c59cdf256b28c1a3b20ba0839397d82b1
Arthur B <arthur@berzieri.fr>
2018-06-11 09:35:48 +0200

    Fixed templating core

--- core/inc_component.php
+++ core/inc_component.php
@@ -10,15 +10,15 @@ class IncComponent {
         $this->cmp_main_path = $this->folder_path . '/' . $this->component_name . '.php';
         $this->cmp_main_js_path = $this->folder_path . '/assets/js/' . $this->component_name . '.js';
         $this->cmp_main_css_path = $this->folder_path . '/assets//css/' . $this->component_name . '.css';
-            
+
         $cmp_main = file_exists($this->cmp_main_path);
         
         if($cmp_main) {
             if(file_exists($this->cmp_main_js_path)) {
-                echo '<script src="' . $this->cmp_main_js_path . '"></script>';    
+                echo '<script src="/' . $this->cmp_main_js_path . '"></script>';
             }
             if(file_exists($this->cmp_main_css_path)) {
-                echo '<link rel="stylesheet" href="' . $this->cmp_main_css_path . '">';    
+                echo '<link rel="stylesheet" href="/' . $this->cmp_main_css_path . '">';
             }
             
             include $this->cmp_main_path;
