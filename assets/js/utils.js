c42056094b45a8067fee0d082ae52965e94ff9fc
Arthur B <arthur@berzieri.fr>
2018-06-14 03:20:23 +0200

    Usefull functions

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -1,24 +1,35 @@
-var isValueValid = function(val, type) {
+var isValueValid = function (val, type) {
   var regexp = {
     textfield: /[\w\d]{3,255}/,
     password: /(.*){6,255}/,
-    email : /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    email: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
   }
 
   return regexp[type].test(val);
 };
-var checkEmail = function(val){
-    return isValueValid(val, "email");
+
+var getUriParams = function () {
+  var result = {};
+  var searchParams = window.location.search.replace('?', '').split('&');
+  for (var i = 0; i < searchParams.length; i++) {
+    var searchParam = searchParams[i].split('=');
+    result[searchParam[0]] = searchParam[1];
+  }
+  return result;
+};
+
+var checkEmail = function (val) {
+  return isValueValid(val, "email");
 }
 
-var checkPassword = function(val){
-    return isValueValid(val, "password");
+var checkPassword = function (val) {
+  return isValueValid(val, "password");
 }
 
-var checkTextField = function(val){
-    return isValueValid(val, "textfield");
+var checkTextField = function (val) {
+  return isValueValid(val, "textfield");
 }
-var getGroupTeam = function(teamName) {
+var getGroupTeam = function (teamName) {
   var teamsGroup = {
     "Russia": "A",
     "Saudi Arabia": "A",
