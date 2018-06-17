69e368f263fc56c719c6b297957946759234b28d
Arthur B <arthur@berzieri.fr>
2018-06-17 02:57:10 +0200

    WIP : Little responsive fixes

--- app/pages/404.php
+++ app/pages/404.php
@@ -1,6 +1,6 @@
 <div class="row full-height">
   <?php new IncComponent("nav_main"); ?>
-  <div class="col-9 main-container error-404">
+  <div class="col-sm-12 col-sm-12 col-md-9 col-lg-9 col-xl-9 main-container error-404">
       &nbsp;
   </div>
 </div>
